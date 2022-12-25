2cebecd4b9b3dc19e3d6fdedd79c07bb6228d195
shit, it's not google <k1merpro@gmail.com>
2022-12-25 17:29:30 +0700

    Добавляет репозиторий лабораторной работы

---
+++ file_io_front/src/components/fileSystem/FileRecord.tsx
@@ -0,0 +1,35 @@
+import { FC } from 'react';
+import { FileService, TFileObject } from '../../services';
+
+
+interface FileRecordProps {
+    record: TFileObject;
+};
+
+export const FileRecord: FC<FileRecordProps> = ({ record }) => {
+    return (
+        <div className = { 'record ' + ( record.isFolder ? 'folder' : 'file' ) }>
+            {
+                record.isFolder 
+                ? <button className = { 'folderName' }
+                    onClick = { async () => await FileService.openFolder( record.name ) }
+                    children = { record.name }
+                />
+                : <p className = 'fileName'>
+                    { record.name }
+                </p>
+            }
+            <div className = 'options'>
+                <button title = 'Переименовать' className = 'rename' onClick = { async () => await FileService.renameFile( record.name ) }/>
+                { 
+                    !record.isFolder 
+                    && <button title = 'Скачать' className = 'download' onClick = { async () => await FileService.downloadFile( record.name ) }/>
+                }
+                { 
+                    record.isFolder 
+                    && <button title = 'Удалить' className = 'remove' onClick = { async () => await FileService.deleteFolder( record.name ) }/>
+                }
+            </div>
+        </div>
+    );
+}
